011ebf7e21b3d25b0244b3d9244b101f44b22e91
HazemEl <h.elkhalil@hotmail.com>
2020-05-01 19:37:36 +0200

    Removed the config api (temporary)

--- src/main/java/se/hazem/hazemapi/HazemApi.java
+++ src/main/java/se/hazem/hazemapi/HazemApi.java
@@ -1,15 +1,8 @@
 package se.hazem.hazemapi;
 
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import se.hazem.hazemapi.gui.GuiHandler;
 import se.hazem.hazemapi.test.Event;
-
-import java.io.File;
-import java.io.IOException;
 
 public final class HazemApi extends JavaPlugin {
     public static HazemApi instance;
